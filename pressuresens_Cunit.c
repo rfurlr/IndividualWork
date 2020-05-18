50d0263b210a9021d24acda303a00f3f39027ed7
rfurlr <65356353+rfurlr@users.noreply.github.com>
2020-05-17 20:34:30 -0500

    Update pressuresens_Cunit.c

--- pressuresens_Cunit.c
+++ pressuresens_Cunit.c
@@ -1,10 +1,38 @@
 
 
 
+void test_senserror (void){
+  CU_ASSERT(senserror(20) == 30);
+  CU_ASSERT(senserror(10) == 15);
+  CU_ASSERT(senserror(2) == 0);
+}
 
+void test_sensread (void){
+  CU_ASSERT(sensread (20,10) == 15);
+  CU_ASSERT(sensread (10,10) == 10);
+  CU_ASSERT(sensread (30,50) == 40);
+}
 
 void test_truesensread (void){
   CU_ASSERT(truesensread(20,15) == 35);
   CU_ASSERT(truesensread(20,0) == 20);
   CU_ASSERT(truesensread(15,15) == 30);
 }
+
+int main(){
+    CU_initialize_registry();
+    CU_pSuite suite = CU_add_suite("test_senserror", 0, 0);
+    CU_pSuite suite = CU_add_suite("test_sensread", 0, 0);
+    CU_pSuite suite = CU_add_suite("test_truesensread", 0, 0);
+
+    CU_add_test(suite, "test_senserror", test_senserror);
+    CU_add_test(suite, "test_sensread", test_sensread);
+    CU_add_test(suite, "test_truesensread", test_truesensread);
+
+    CU_basic_set_mode(CU_BRM_VERBOSE);
+    CU_basic_run_tests();
+    CU_cleanup_registry();
+
+    return 0;
+}
+
