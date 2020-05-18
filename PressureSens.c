d3985ca800128e6dd73953e7cb572c03f90367e9
rfurlr <65356353+rfurlr@users.noreply.github.com>
2020-05-17 20:03:31 -0500

    Update PressureSens.c

--- PressureSens.c
+++ PressureSens.c
@@ -2,7 +2,11 @@
 #include <stdlib.h>
 #include "CUnit.h"
 #include "Basic.h"
+
 int errormarg;
+int sensreading;
+int truuread;
+
 int senserror (int SensAge){
   
   if (SensAge > 15){
@@ -23,3 +27,27 @@ void errortest (void){
   CU_ASSERT(senserror(2) == 0);
 }
 
+ int sensread (int press1, int press2){
+  int sum = press1 + press2;
+  sensereading  = sum / 2.0;
+ }
+ 
+void readtest (void){
+  CU_ASSERT(sensread (20,10) == 15);
+  CU_ASSERT(sensread (10,10) == 10);
+  CU_ASSERT(sensread (30,50) == 40);
+}
+
+int truesensread(errormarg, sensreading){
+  truuread = errormarg + sensreading;
+}
+
+void truereadtest (void){
+  CU_ASSERT(truesensread(20,15) == 35);
+  CU_ASSERT(truesensread(20,0) == 20);
+  CU_ASSERT(truesensread(15,15) == 30);
+}
+
+int main (){
+  
+}
