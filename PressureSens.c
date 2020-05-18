5744652a1129f5a50db544d7dd4995475a3e435d
rfurlr <65356353+rfurlr@users.noreply.github.com>
2020-05-17 19:16:00 -0500

    Update PressureSens.c

--- PressureSens.c
+++ PressureSens.c
@@ -3,5 +3,18 @@
 #include "CUnit.h"
 #include "Basic.h"
 
+int senserror (int SensAge){
+  int errormarg;
+  if (SensAge > 15){
+    errormarg = 30;
+  }
+  else if (15 > SensAge > 5){
+    errormarg = 15;
+  }
+  else if(SensAge < 5){
+    errormarg = 0:
+  }
+  return errormarg;
+}
 
 
