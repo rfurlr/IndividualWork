b9a90dd1ee0d2fb3b7156d6579951b689238aa08
rfurlr <65356353+rfurlr@users.noreply.github.com>
2020-05-18 19:58:40 -0500

    Update Main.c

--- Main.c
+++ Main.c
@@ -2,12 +2,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int errormarg;
+int sensreading;
 int SensAge = 20;
 int press1 = 20;
 int press2 = 10;
 
 int main (){
-  senserror(SensAge);
-  sensread(press1, press2);
-  truesensread(errormarg, sensreading);
+  int senserror(SensAge);
+  int sensread(press1, press2);
+  int truesensread(errormarg, sensreading);
 }
